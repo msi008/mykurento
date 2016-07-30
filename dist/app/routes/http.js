045a2d2536db0f667c7e188b670d9fa85f1c4dbf
fugang <msi_008@163.com>
2016-07-30 14:05:57 +0800

    remove dis

--- dist/app/routes/http.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// TODO refactor to use global configuration
-router.get('/:room', function(req, res) {
-	res.sendFile('room.html', { root : './public' });
-});
-
-module.exports = router;
